ebdfce2f3bab2aba8a07a100a3b1192df5b35cc9
Cong Dao <congdaovan94@gmail.com>
2020-10-14 17:02:00 -0400

    Updates

---
+++ precache-manifest.a67e040109d782b0cc0fe473cdb03f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c549b058e36e7e6ffaa625b92498b482",
+    "url": "/snake-clone/index.html"
+  },
+  {
+    "revision": "81dcd757b30f7f04ae6e",
+    "url": "/snake-clone/static/css/main.eec7b90a.chunk.css"
+  },
+  {
+    "revision": "67e274b23c8f3b993dce",
+    "url": "/snake-clone/static/js/2.e884a423.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-clone/static/js/2.e884a423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dcd757b30f7f04ae6e",
+    "url": "/snake-clone/static/js/main.9d128676.chunk.js"
+  },
+  {
+    "revision": "7b250344f7a1da6f3c47",
+    "url": "/snake-clone/static/js/runtime-main.241e579a.js"
+  }
+]);
